1fca99d8cae80d42706ccfe8616426363828881f
Eduardo510 <56281271+Eduardo510@users.noreply.github.com>
2022-09-13 20:55:40 -0600

    Final

--- js/trackingfbpixel.js
+++ js/trackingfbpixel.js
@@ -1,1 +1,2 @@
+/*Eduardo Daniel Navarro Monterroso - 19008451*/
 var facebook_id=php_varsfb;!function(e,o,t,a,n,c,f){e.fbq||(n=e.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)},e._fbq||(e._fbq=n),n.push=n,n.loaded=!0,n.version="2.0",n.queue=[],(c=o.createElement(t)).async=!0,c.src="https://connect.facebook.net/en_US/fbevents.js",(f=o.getElementsByTagName(t)[0]).parentNode.insertBefore(c,f))}(window,document,"script"),void 0===facebook_id||""===facebook_id||(fbq("init",facebook_id),fbq("track","PageView"));
